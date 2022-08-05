64aa24d3d0dc000cc03363e4ba4643638178c349
FaisalMS <fis445@hotmail.com>
2022-08-05 15:05:28 +0300

    first commit

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -52,10 +52,11 @@ class MainActivity : AppCompatActivity() {
         setViewElements()
         lemonImage!!.setOnClickListener {
             // TODO: call the method that handles the state when the image is clicked
+            clickLemonImage()
         }
         lemonImage!!.setOnLongClickListener {
             // TODO: replace 'false' with a call to the function that shows the squeeze count
-            false
+            showSnackbar()
         }
     }
 
@@ -80,22 +81,24 @@ class MainActivity : AppCompatActivity() {
         //  when the image is clicked we may need to change state to the next step in the
         //  lemonade making progression (or at least make some changes to the current state in the
         //  case of squeezing the lemon). That should be done in this conditional statement
-
-        // TODO: When the image is clicked in the SELECT state, the state should become SQUEEZE
-        //  - The lemonSize variable needs to be set using the 'pick()' method in the LemonTree class
-        //  - The squeezeCount should be 0 since we haven't squeezed any lemons just yet.
-
-        // TODO: When the image is clicked in the SQUEEZE state the squeezeCount needs to be
-        //  INCREASED by 1 and lemonSize needs to be DECREASED by 1.
-        //  - If the lemonSize has reached 0, it has been juiced and the state should become DRINK
-        //  - Additionally, lemonSize is no longer relevant and should be set to -1
-
-        // TODO: When the image is clicked in the DRINK state the state should become RESTART
-
-        // TODO: When the image is clicked in the RESTART state the state should become SELECT
-
+        when (lemonadeState) {
+            // TODO: When the image is clicked in the SELECT state, the state should become SQUEEZE
+            //  - The lemonSize variable needs to be set using the 'pick()' method in the LemonTree class
+            //  - The squeezeCount should be 0 since we haven't squeezed any lemons just yet.
+            SELECT -> { lemonadeState = SQUEEZE ;lemonSize = lemonTree.pick() ;squeezeCount = 0 }
+            // TODO: When the image is clicked in the SQUEEZE state the squeezeCount needs to be
+            //  INCREASED by 1 and lemonSize needs to be DECREASED by 1.
+            //  - If the lemonSize has reached 0, it has been juiced and the state should become DRINK
+            //  - Additionally, lemonSize is no longer relevant and should be set to -1
+            SQUEEZE -> { squeezeCount += 1 ;lemonSize -= 1 ;if (lemonSize == 0) { lemonadeState = DRINK ;lemonSize = -1} }
+            // TODO: When the image is clicked in the DRINK state the state should become RESTART
+            DRINK -> { lemonadeState = RESTART }
+            // TODO: When the image is clicked in the RESTART state the state should become SELECT
+            RESTART -> { lemonadeState = SELECT }
+        }
         // TODO: lastly, before the function terminates we need to set the view elements so that the
         //  UI can reflect the correct state
+        setViewElements()
     }
 
     /**
@@ -103,7 +106,6 @@ class MainActivity : AppCompatActivity() {
      */
     private fun setViewElements() {
         val textAction: TextView = findViewById(R.id.text_action)
-        // TODO: set up a conditional that tracks the lemonadeState
 
         // TODO: for each state, the textAction TextView should be set to the corresponding string from
         //  the string resources file. The strings are named to match the state
@@ -111,6 +113,12 @@ class MainActivity : AppCompatActivity() {
         // TODO: Additionally, for each state, the lemonImage should be set to the corresponding
         //  drawable from the drawable resources. The drawables have the same names as the strings
         //  but remember that they are drawables, not strings.
+        when(lemonadeState){
+            SELECT -> { textAction.text = "اقطف من شجرة الليمون \uD83C\uDF4B" ; lemonImage?.setImageResource(R.drawable.lemon_tree) }
+            SQUEEZE -> { textAction.text = "اعصر الليمون" ;lemonImage?.setImageResource(R.drawable.lemon_squeeze) }
+            DRINK -> { textAction.text = "اشرب الليمون \uD83E\uDD64" ;lemonImage?.setImageResource(R.drawable.lemon_drink) }
+            RESTART -> { textAction.text = "خلص العصير\uD83D\uDE05 عيد التجربة" ;lemonImage?.setImageResource(R.drawable.lemon_restart) }
+        }
     }
 
     /**
